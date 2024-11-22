4ee92a7f5960869cb92a386bec64bc442acda027
eliy5550 <eliy5550@gmail.com>
2024-11-22 22:55:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ app.use(express.static("./public"))
 
 app.use(express.static("./well-known"))
 
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
 
-app.listen(80 , ()=>{
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
-    console.log("server running... 80");
-})
-app.listen(443 , ()=>{
 
-    console.log("server running... 443");
-})
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
