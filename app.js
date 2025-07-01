e68357011a0e43485d012d561b585c44a67727dc
eliy5550 <eliy5550@gmail.com>
2025-07-01 11:33:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,34 +7,38 @@ const express = require('express')
 const app = express()
 
 
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+// app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
 app.use(express.static("./public"))
 
-app.use(express.static("./well-known"))
+// app.use(express.static("./well-known"))
 
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+// // Certificate
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
 
 
 // Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+//const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(credentials, app);
 
 
 
-httpServer.listen(80, () => {
+app.listen(80, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
+app.listen(433, () => {
 	console.log('HTTPS Server running on port 443');
 });
+
+// httpsServer.listen(443, () => {
+// 	console.log('HTTPS Server running on port 443');
+// });
